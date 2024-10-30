049990600a3d9e824c7af4e371f6571ee3c9dd1e
Inês Martins <inesfmartins@ua.pt>
2024-10-30 12:28:10 +0000

    adicionar tarefas + check tarefas

--- ex1_react/src/App.js
+++ ex1_react/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/TodoForm'
+import TodoForm from './components/TodoForm';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [tasks, setTasks]= useState([]); // var de estado que guarda as tarefas adicionadas
+  const [selectedTasks, setSelectedTasks] = useState([]); // var de estado que guarda as tarefas concluídas
+
+  //função de callback - tarefas adicionadas
+  const setdados = (dadosFilho) => { 
+    setTasks([...tasks, dadosFilho]);  
+  }
+
+  //função que define a seleção das tarefas
+  const handleCheckboxChange = (event) => {
+
+  const checkedTask = event.target.value;
+
+  if(event.target.checked){
+   setSelectedTasks([...selectedTasks, checkedTask])
+  }else{
+   setSelectedTasks(selectedTasks.filter(id=>id !== checkedTask)) //
+  }
+  }
+
+  console.log("array tarefas:", tasks);
+  console.log("array tarefas concluídas:", selectedTasks);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoForm setdados={setdados} />
+
+      {tasks.map(item => (
+          <label key={item.id}>
+            <input type='checkbox' 
+            value={item}
+            checked={selectedTasks.includes(item)}
+            onChange={(event) => { handleCheckboxChange(event) }}
+            />
+            {item}
+          </label>
+        ))}
+
     </div>
   );
 }
---
+++ ex1_react/src/components/TodoForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function TodoForm(props) {
+
+    const [input, setInput] = useState(''); 
+
+    return (
+      <div>
+        <p>What needs to be done?</p>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)} //atualiza o valor do input
+        />        
+        <button 
+        onClick={() => {
+            props.setdados(input); // adiciona uma nova tarefa com o  valor do input
+            setInput(''); //limpa o input depois de adicionar a tarefa
+        }}        
+        >
+            Add
+        </button>
+      </div>
+    );
+  }
+
+  export default TodoForm;
